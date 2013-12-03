8d0d6ab4311390e79964d0e514813bb436696202
OliverNielsen <OliverNielsen1993@gmail.com>
2013-12-03 18:02:17 +0100

    New SELECT procedures with parameters is done
    - Oliver Nielsen

--- SQL/CRUD_Seperate/EasyTeach_Select.sql
+++ SQL/CRUD_Seperate/EasyTeach_Select.sql
@@ -1,6 +1,6 @@
 /* Author: 	Oliver Nielsen
- * Date: 	30. November, 2013
- * Version:	1.01
+ * Date: 	03. December, 2013
+ * Version:	1.1
 
  * Following is the SQL code (DML) for manipulating the tables for the 
  * "MBO EasyTeach" application.
@@ -72,8 +72,8 @@ DELIMITER ;
 
 /* Returns the class with the specific classNo */
 DELIMITER //
-CREATE PROCEDURE selectClassRowsWithClassNo (
-	IN pClassNo			INTEGER(6))
+CREATE PROCEDURE selectClassRowWithClassNo (
+	IN pClassNo				INTEGER(6))
 BEGIN
 	SELECT c.*
 		FROM Class c
@@ -92,8 +92,8 @@ DELIMITER ;
 
 /* Returns the course with the specific courseNo */
 DELIMITER //
-CREATE PROCEDURE selectCourseRows (
-	IN pCourseNo		INTEGER(6))
+CREATE PROCEDURE selectCourseRowWithCourseNo (
+	IN pCourseNo			INTEGER(6))
 BEGIN
 	SELECT c.*
 		FROM Course c
@@ -112,8 +112,8 @@ DELIMITER ;
 
 /* Returns the user with the specific userNo */
 DELIMITER //
-CREATE PROCEDURE selectUserRows (
-	IN pUserNo		INTEGER(6))
+CREATE PROCEDURE selectUserRowWithUserNo (
+	IN pUserNo				INTEGER(6))
 BEGIN
 	SELECT u.*
 		FROM User u
@@ -132,11 +132,12 @@ DELIMITER ;
 
 /* Returns the question with the specific questionNo */
 DELIMITER //
-CREATE PROCEDURE selectQuestionRows (
-	IN pQuestionNo		INTEGER(6))
+CREATE PROCEDURE selectQuestionRowWithQuestionNo (
+	IN pQuestionNo			INTEGER(6))
 BEGIN
 	SELECT q.*
-		FROM Question q;
+		FROM Question q
+		WHERE q.questionNo = pQuestionNo;
 END //
 DELIMITER ;
 
@@ -149,30 +150,58 @@ BEGIN
 END //
 DELIMITER ;
 
-/* Returns every answer */
+/* Returns the tag with the specific tagNo */
 DELIMITER //
-CREATE PROCEDURE selectAnswerRows ()
+CREATE PROCEDURE selectTagRowWithTagNo (
+	IN pTagNo				INTEGER(6))
+BEGIN
+	SELECT t.*
+		FROM Tag t
+		WHERE t.tagNo = pTagNo;
+END //
+DELIMITER ;
+
+/* Returns the answer for the specific questionNo */
+DELIMITER //
+CREATE PROCEDURE selectAnswerRowWithQuestionNo (
+	IN pQuestionNo			INTEGER(6))
 BEGIN
 	SELECT a.*
-		FROM Answer a;
+		FROM Answer a
+		WHERE a.questionNo = pQuestionNo;
 END //
 DELIMITER ;
 
-/* Returns every exericse parameter */
+/* Returns the exercise parameter with the specific exerciseParameterNo */
 DELIMITER //
-CREATE PROCEDURE selectExerciseParameterRows ()
+CREATE PROCEDURE selectExerciseParameterRowWithExerciseParameterNo (
+	IN pExerciseParameterNo	INTEGER(10))
 BEGIN
 	SELECT ep.*
-		FROM ExerciseParameter ep;
+		FROM ExerciseParameter ep
+		WHERE ep.exerciseParameterNo = pExerciseParameterNo;
 END //
 DELIMITER ;
 
-/* Returns every exercise */
+/* Returns the exercise with the specific exerciseNo */
 DELIMITER //
-CREATE PROCEDURE selectExerciseRows ()
+CREATE PROCEDURE selectExerciseRowWithExerciseNo (
+	IN pExerciseNo			INTEGER(6))
 BEGIN
 	SELECT e.*
-		FROM Exercise e;
+		FROM Exercise e
+		WHERE e.exerciseNo = pExerciseNo;
+END //
+DELIMITER ;
+
+/* Returns the exercise for the specific courseNo */
+DELIMITER //
+CREATE PROCEDURE selectExerciseRowWithCourseNo (
+	IN pCourseNo			INTEGER(6))
+BEGIN
+	SELECT e.*
+		FROM Exercise e
+		WHERE e.courseNo = pCourseNo;
 END //
 DELIMITER ;
 
@@ -185,12 +214,56 @@ BEGIN
 END //
 DELIMITER ;
 
+/* Returns the class course relations with the specific classNo */
+DELIMITER //
+CREATE PROCEDURE selectClassCourseRelationRowsWithClassNo (
+	IN pClassNo				INTEGER(6))
+BEGIN
+	SELECT ccr.*
+		FROM ClassCourseRelation ccr
+		WHERE ccr.classNo = pClassNo;
+END //
+DELIMITER ;
+
+/* Returns the class course relations with the specific courseNo */
+DELIMITER //
+CREATE PROCEDURE selectClassCourseRelationRowsWithCourseNo (
+	IN pCourseNo			INTEGER(6))
+BEGIN
+	SELECT ccr.*
+		FROM ClassCourseRelation ccr
+		WHERE ccr.courseNo = pCourseNo;
+END //
+DELIMITER ;
+
 /* Returns every user test result */
 DELIMITER //
 CREATE PROCEDURE selectUserTestResultRows ()
 BEGIN
 	SELECT utr.*
 		FROM UserTestResult utr;
+END //
+DELIMITER ;
+
+/* Returns the user test results for the specific userNo */
+DELIMITER //
+CREATE PROCEDURE selectUserTestResultRowsWithUserNo (
+	IN pUserNo				INTEGER(6))
+BEGIN
+	SELECT utr.*
+		FROM UserTestResult utr
+		WHERE utr.userNo = pUserNo;
+END //
+DELIMITER ;
+
+/* Returns the user test results with the specific exerciseNo */
+DELIMITER //
+CREATE PROCEDURE selectUserTestResultRowsWithExerciseNo (
+	IN pExerciseNo			INTEGER(6))
+BEGIN
+	SELECT utr.*
+		FROM UserTestResult utr
+		WHERE utr.exerciseNo = pExerciseNo;
 END //
 DELIMITER ;
 
@@ -203,12 +276,56 @@ BEGIN
 END //
 DELIMITER ;
 
+/* Returns the class user relations with the specific classNo */
+DELIMITER //
+CREATE PROCEDURE selectClassUserRelationRowsWithClassNo (
+	IN pClassNo				INTEGER(6))
+BEGIN
+	SELECT cur.*
+		FROM ClassUserRelation cur
+		WHERE cur.classNo = pClassNo;
+END //
+DELIMITER ;
+
+/* Returns the class user relations for the specific UserNo */
+DELIMITER //
+CREATE PROCEDURE selectClassUserRelationRowsWithUserNo (
+	IN pUserNo				INTEGER(6))
+BEGIN
+	SELECT cur.*
+		FROM ClassUserRelation cur
+		WHERE cur.userNo = pUserNo;
+END //
+DELIMITER ;
+
 /* Returns every user question state */
 DELIMITER //
 CREATE PROCEDURE selectUserQuestionStateRows ()
 BEGIN
 	SELECT uqs.*
 		FROM UserQuestionState uqs;
+END //
+DELIMITER ;
+
+/* Returns the user question states for the specific userNo */
+DELIMITER //
+CREATE PROCEDURE selectUserQuestionStateRowsWithUserNo (
+	IN pUserNo				INTEGER(6))
+BEGIN
+	SELECT uqs.*
+		FROM UserQuestionState uqs
+		WHERE uqs.userNo = pUserNo;
+END //
+DELIMITER ;
+
+/* Returns the user question states with the specific questionNo */
+DELIMITER //
+CREATE PROCEDURE selectUserQuestionStateRowsWithQuestionNo (
+	IN pQuestionNo			INTEGER(6))
+BEGIN
+	SELECT uqs.*
+		FROM UserQuestionState uqs
+		WHERE uqs.questionNo = pQuestionNo;
 END //
 DELIMITER ;
 
@@ -221,12 +338,56 @@ BEGIN
 END //
 DELIMITER ;
 
+/* Returns the question tag relations with the specific questionNo */
+DELIMITER //
+CREATE PROCEDURE selectQuestionTagRelationRowsWithQuestionNo (
+	IN pQuestionNo			INTEGER(6))
+BEGIN
+	SELECT qtr.*
+		FROM QuestionTagRelation qtr
+		WHERE qtr.questionNo = pQuestionNo;
+END //
+DELIMITER ;
+
+/* Returns the question tag relations with the specific tagNo */
+DELIMITER //
+CREATE PROCEDURE selectQuestionTagRelationRowsWithTagNo (
+	IN pTagNo				INTEGER(6))
+BEGIN
+	SELECT qtr.*
+		FROM QuestionTagRelation qtr
+		WHERE qtr.tagNo = pTagNo;
+END //
+DELIMITER ;
+
 /* Returns every exercise question relation */
 DELIMITER //
 CREATE PROCEDURE selectExerciseQuestionRelationRows ()
 BEGIN
 	SELECT eqr.*
 		FROM ExerciseQuestionRelation eqr;
+END //
+DELIMITER ;
+
+/* Returns the exercise question relations with the specific exerciseNo */
+DELIMITER //
+CREATE PROCEDURE selectExerciseQuestionRelationRowsWithExerciseNo (
+	IN pExerciseNo			INTEGER(6))
+BEGIN
+	SELECT eqr.*
+		FROM ExerciseQuestionRelation eqr
+		WHERE eqr.exerciseNo = pExerciseNo;
+END //
+DELIMITER ;
+
+/* Returns the exercise question relations with the specific questionNo */
+DELIMITER //
+CREATE PROCEDURE selectExerciseQuestionRelationRowsWithQuestionNo (
+	IN pQuestionNo			INTEGER(6))
+BEGIN
+	SELECT eqr.*
+		FROM ExerciseQuestionRelation eqr
+		WHERE eqr.questionNo = pQuestionNo;
 END //
 DELIMITER ;
 
