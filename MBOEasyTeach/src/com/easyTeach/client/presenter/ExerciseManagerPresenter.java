57db7926c88d6e8e7eeeda43bb4cee29ddb21ee7
warreq <brandon.s.lucas@gmail.com>
2013-12-15 01:59:27 +0100

    ExerciseManager added
    Stripped down out of necessity. We currently have no way to get
    questions for a given exercise without directly calling the db
    wrapper. And I couldn't put all the columns in the static ui
    in without making calls to the db in the TableModel, per row. Ew.

--- MBOEasyTeach/src/com/easyTeach/client/presenter/ExerciseManagerPresenter.java
+++ MBOEasyTeach/src/com/easyTeach/client/presenter/ExerciseManagerPresenter.java
@@ -1,5 +1,216 @@
 package com.easyTeach.client.presenter;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.UUID;
+
+import com.easyTeach.client.network.EasyTeachClient;
+import com.easyTeach.common.entity.Exercise;
+import com.easyTeach.common.entity.Resource;
+import com.easyTeach.common.entity.ResourceSet;
+import com.easyTeach.common.network.Action;
+import com.easyTeach.common.network.Action.ActionType;
+import com.easyTeach.common.network.Request;
+import com.easyTeach.common.network.Response.ResponseStatus;
+import com.easyTeach.common.network.Session;
+
+/**
+ * The ExerciseManagerPresenter communicates with the domain logic on behalf of
+ * the ExerciseManagerUI, providing all of its logic.
+ * <p>
+ * The Listeners in the UI class call the relevant methods in the Presenter in
+ * order to update and retrieve information from the domain logic. This is an
+ * implementation of the Model View Presenter pattern.
+ * </p>
+ * 
+ * @author Brandon Lucas
+ * @version 1.0
+ * @date 14. December, 2013
+ */
 public class ExerciseManagerPresenter {
 
+	private ExerciseTableModel exerciseTable;
+	protected ResourceSet filteredSelectionSet;
+	protected ResourceSet exerciseSet;
+	private EasyTeachClient client;
+	private Exercise currentlySelectedExercise;
+	private boolean isFiltered;
+
+	private String[] exerciseColumnHeaders = { "Exercise Name", "Date added" };
+
+	/**
+	 * Initializes sets and table models
+	 */
+	public ExerciseManagerPresenter() {
+		this.exerciseSet = new ResourceSet();
+
+		this.exerciseTable = new ExerciseTableModel(this.exerciseColumnHeaders,
+				this.exerciseSet);
+
+		refreshTable();
+	}
+
+	public ExerciseTableModel getExerciseTableModel() {
+		return this.exerciseTable;
+	}
+
+	public void setSelectedExercise(int row) {
+		this.currentlySelectedExercise = (Exercise) this.exerciseTable
+				.getResourceAtRow(row);
+	}
+
+	/**
+	 * Sends a {@link Request} to the Server, updating the Presenter's set of
+	 * Exercises.
+	 * 
+	 * <p>
+	 * It checks if the data in the table should be filtered, and updates the
+	 * Table Model accordingly, before firing the dataChanged event and forcing
+	 * the table in the UI to update.
+	 * </p>
+	 */
+	private void refreshTable() {
+
+		Action toDo = new Action(ActionType.READ, "all");
+
+		Request getExercises = new Request(Session.getInstance(), toDo,
+				new Exercise());
+		this.client = new EasyTeachClient(getExercises);
+		this.client.run();
+		this.client.getResponse();
+		if (this.client.getResponse().getStatus() != ResponseStatus.FAILURE) {
+			this.exerciseSet = (ResourceSet) this.client.getResponse()
+					.getResponse();
+		}
+		if (this.isFiltered) {
+			this.exerciseTable.refreshData(this.filteredSelectionSet);
+		} else {
+			this.exerciseTable.refreshData(this.exerciseSet);
+		}
+
+		this.exerciseTable.fireTableDataChanged();
+	}
+
+	/**
+	 * Logic for filtering the table containing the selection of students by a
+	 * particular string, in a particular column.
+	 * 
+	 * <p>
+	 * Right now, this consists of maintaining a separate, filtered set. This is
+	 * somewhat slow. Later, new logic should be added, using RowFilter.
+	 * </p>
+	 * 
+	 * @param column
+	 *            The column in the table which should be considered for
+	 *            filtering
+	 * @param by
+	 *            The string by which the aforementioned column should be
+	 *            filtered.
+	 */
+	public void filter(String column, String by) {
+		if (by.equals("")) {
+			this.isFiltered = false;
+		} else {
+			this.isFiltered = true;
+			this.filteredSelectionSet = new ResourceSet();
+			switch (column) {
+			case "Author":
+				for (Resource r : this.exerciseSet) {
+					Exercise e = (Exercise) r;
+					if (e.getAuthor().contains(by)) {
+						this.filteredSelectionSet.add(e);
+					}
+				}
+				break;
+			case "Exercise Name":
+				for (Resource r : this.exerciseSet) {
+					Exercise e = (Exercise) r;
+					if (e.getExerciseName().contains(by)) {
+						this.filteredSelectionSet.add(e);
+					}
+				}
+				break;
+			}
+		}
+		this.refreshTable();
+		this.exerciseTable.refreshData(this.filteredSelectionSet);
+		this.exerciseTable.fireTableDataChanged();
+	}
+
+	public void duplicate() {
+		if (this.currentlySelectedExercise != null) {
+			String newCourseId = UUID.randomUUID().toString();
+			Exercise newExercise = this.currentlySelectedExercise;
+			newExercise.setExerciseNo(newCourseId);
+			GregorianCalendar cal = new GregorianCalendar();
+			newExercise.setDateAdded(new Timestamp(cal.getTimeInMillis()));
+
+			Action createExercise = new Action(ActionType.CREATE);
+			Request dupeExercise = new Request(Session.getInstance(),
+					createExercise, newExercise);
+
+			this.client = new EasyTeachClient(dupeExercise);
+			this.client.run();
+
+			this.refreshTable();
+			this.exerciseTable.refreshData(this.exerciseSet);
+			this.exerciseTable.fireTableDataChanged();
+		}
+	}
+
+	/**
+	 * Removes a User from the EnrolledTable.
+	 */
+	public void delete() {
+		if (this.currentlySelectedExercise != null) {
+			Action rm = new Action(ActionType.DELETE);
+			Request rmExercise = new Request(Session.getInstance(),
+					rm, this.currentlySelectedExercise);
+
+			this.client = new EasyTeachClient(rmExercise);
+			this.client.run();
+			this.refreshTable();
+			this.exerciseTable.refreshData(this.exerciseSet);
+			this.exerciseTable.fireTableDataChanged();
+		}
+	}
+
+	/**
+	 * 
+	 * @author Brandon Lucas
+	 * @version 1.0
+	 * @date 14 December, 2013
+	 * 
+	 *       Presenter-specific implementation of the abstract
+	 *       DisplayTableModel.
+	 * 
+	 *       <p>
+	 *       Implementation of the DisplayTableModel on a per-Presenter basis
+	 *       allows us to interact rows in the table model as actual domain
+	 *       objects, specific to the demands of the particular user-interface.
+	 *       </p>
+	 * 
+	 */
+	private class ExerciseTableModel extends DisplayTableModel {
+		public ExerciseTableModel(String[] columnHeaders, ResourceSet resources) {
+			super(columnHeaders, resources);
+		}
+
+		private static final long serialVersionUID = -2996998684634631118L;
+
+		@Override
+		public String getValueAt(int row, int column) {
+			Exercise e = (Exercise) this.tableData.get(row);
+			switch (this.getColumnName(column)) {
+			case "Exercise Name":
+				return e.getExerciseName();
+			case "Date added":
+				return e.getDateAdded().toString();
+			default:
+				return new String();
+			}
+		}
+	}
 }
--- MBOEasyTeach/src/com/easyTeach/client/ui/ExerciseManagerUI.java
+++ MBOEasyTeach/src/com/easyTeach/client/ui/ExerciseManagerUI.java
@@ -4,9 +4,10 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
@@ -19,9 +20,7 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
-import javax.swing.table.DefaultTableModel;
 
-import com.easyTeach.client.presenter.DisplayTableModel;
 import com.easyTeach.client.presenter.ExerciseManagerPresenter;
 import com.easyTeach.client.presenter.HelpPresenter;
 import com.easyTeach.common.ui.UIColors;
@@ -37,259 +36,239 @@ import com.easyTeach.common.ui.UIColors;
 
 public class ExerciseManagerUI {
 
-    private final String[] FILTER = { "Author", "Exercise Name",
-            "Question Tag", "Is Test", "Course" };
-    private JPanel exerciseManagerPanel;
-    JButton btnHelp;
-    JButton btnEditExercise;
-    JButton btnDeleteExercise;
-    JButton btnAddExercise;
-    JPanel centerPanel;
-    JPanel lowerCenterPanel;
-    JTable questionTable;
-    JTable exerciseTable;
-    JPanel filterPanel;
-    JComboBox<String> filterBox;
-    JButton btnFilter;
-    JPanel questionTablePanel;
-    JTextField txtFilter;
-    JButton btnDuplicateExercise;
+	private final String[] FILTER = { "Author", "Exercise Name" };
+	private JPanel exerciseManagerPanel;
+	ExerciseManagerPresenter presenter;
+	JButton btnHelp;
+	JButton btnEditExercise;
+	JButton btnDeleteExercise;
+	JButton btnAddExercise;
+	JPanel centerPanel;
+	JPanel lowerCenterPanel;
+	JTable exerciseTable;
+	JPanel filterPanel;
+	JComboBox<String> filterBox;
+	JButton btnFilter;
+	JPanel questionTablePanel;
+	JTextField txtFilter;
+	JButton btnDuplicateExercise;
 
-    /**
-     * Constructor for building the exerciseManagerPanel. The panel is built by
-     * calling the buildPanel method. The constructor also calls a method adding
-     * actionListeners to all buttons.
-     */
-    public ExerciseManagerUI() {
-        buildPanel();
-        addActionListeners();
-    }
+	/**
+	 * Constructor for building the exerciseManagerPanel. The panel is built by
+	 * calling the buildPanel method. The constructor also calls a method adding
+	 * actionListeners to all buttons.
+	 */
+	public ExerciseManagerUI() {
+		this.presenter = new ExerciseManagerPresenter();
+		buildPanel();
+		addActionListeners();
+	}
 
-    /**
-     * The getExerciseManagerUI returns the JPanel containing all the components
-     * needed to maintain exercises.
-     * 
-     * @return the JPanel, exerciseManagerPanel. Meant to be used in another
-     *         frame.
-     */
-    public JPanel getExerciseManagerUI() {
-        return this.exerciseManagerPanel;
-    }
+	/**
+	 * The getExerciseManagerUI returns the JPanel containing all the components
+	 * needed to maintain exercises.
+	 * 
+	 * @return the JPanel, exerciseManagerPanel. Meant to be used in another
+	 *         frame.
+	 */
+	public JPanel getExerciseManagerUI() {
+		return this.exerciseManagerPanel;
+	}
 
-    /**
-     * The method responsible for building the exerciseManagerPanel. The panel
-     * consists of all the JComponenets needed to maintain exercises.
-     */
-    public void buildPanel() {
-        this.exerciseManagerPanel = new JPanel(new BorderLayout());
-        this.exerciseManagerPanel.setBackground(UIColors.lightBlue);
+	/**
+	 * The method responsible for building the exerciseManagerPanel. The panel
+	 * consists of all the JComponenets needed to maintain exercises.
+	 */
+	public void buildPanel() {
+		this.exerciseManagerPanel = new JPanel(new BorderLayout());
+		this.exerciseManagerPanel.setBackground(UIColors.lightBlue);
 
-        buildCenterPanel();
-        buildSouthPanel();
-    }
+		buildCenterPanel();
+		buildSouthPanel();
+	}
 
-    /**
-     * Method for creating the south panel with buttons for getting help and
-     * editing/deleting/adding exercises.
-     */
-    private void buildSouthPanel() {
-        JPanel southButtonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        southButtonPanel.setBackground(UIColors.darkBlue);
+	/**
+	 * Method for creating the south panel with buttons for getting help and
+	 * editing/deleting/adding exercises.
+	 */
+	private void buildSouthPanel() {
+		JPanel southButtonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		southButtonPanel.setBackground(UIColors.darkBlue);
 
-        this.btnHelp = new JButton("Help");
-        southButtonPanel.add(this.btnHelp);
+		this.btnHelp = new JButton("Help");
+		southButtonPanel.add(this.btnHelp);
 
-        this.btnEditExercise = new JButton("Edit Exercise");
-        southButtonPanel.add(this.btnEditExercise);
+		this.btnEditExercise = new JButton("Edit Exercise");
+		southButtonPanel.add(this.btnEditExercise);
 
-        this.btnDuplicateExercise = new JButton("Duplicate Exercise");
-        southButtonPanel.add(this.btnDuplicateExercise);
-        
-        this.btnDeleteExercise = new JButton("Delete Exercise");
-        southButtonPanel.add(this.btnDeleteExercise);
+		this.btnDuplicateExercise = new JButton("Duplicate Exercise");
+		southButtonPanel.add(this.btnDuplicateExercise);
 
-        this.btnAddExercise = new JButton("Add Exercise");
-        southButtonPanel.add(this.btnAddExercise);
+		this.btnDeleteExercise = new JButton("Delete Exercise");
+		southButtonPanel.add(this.btnDeleteExercise);
 
-        this.exerciseManagerPanel.add(southButtonPanel, BorderLayout.SOUTH);
-    }
+		this.btnAddExercise = new JButton("Add Exercise");
+		southButtonPanel.add(this.btnAddExercise);
 
-    /**
-     * Builds the center panel consisting of two inner panels. One for an
-     * question table showing which exercises relate to questions and another
-     * containing a panel to filter exercises.
-     */
-    private void buildCenterPanel() {
-        this.centerPanel = new JPanel(new GridLayout(2, 1));
-        this.exerciseManagerPanel.add(this.centerPanel, BorderLayout.CENTER);
+		this.exerciseManagerPanel.add(southButtonPanel, BorderLayout.SOUTH);
+	}
 
-        buildUpperCenterPanel();
+	/**
+	 * Builds the filter panel for filtering exercises.
+	 */
+	private void buildFilterPanel() {
+		this.filterPanel = new JPanel();
+		this.filterPanel.setBorder(new TitledBorder(new EtchedBorder(
+				EtchedBorder.LOWERED, new Color(0, 0, 0), null),
+				"Filter Exercises", TitledBorder.CENTER, TitledBorder.TOP,
+				new Font("Tahoma", Font.PLAIN, 20)));
+		this.filterPanel.setBackground(UIColors.lightBlue);
 
-        this.lowerCenterPanel = new JPanel(new BorderLayout());
-        buildFilterPanel();
-        buildQuestionTablePanel();
+		JLabel lblFilter = new JLabel("Filter");
+		this.filterPanel.add(lblFilter);
 
-        this.centerPanel.add(this.lowerCenterPanel);
-    }
+		this.filterBox = new JComboBox<>();
+		this.filterBox.setModel(new DefaultComboBoxModel<>(this.FILTER));
+		this.filterPanel.add(this.filterBox);
 
-    /**
-     * Builds a panel with a table for questions for a selected exercise.
-     */
-    private void buildQuestionTablePanel() {
-        this.questionTablePanel = new JPanel(new BorderLayout());
-        this.questionTable = new JTable();
+		JLabel lblBy = new JLabel("by");
+		this.filterPanel.add(lblBy);
 
-        this.questionTablePanel.setBackground(UIColors.lightBrown);
-        this.questionTablePanel.setBorder(new TitledBorder(new EtchedBorder(
-                EtchedBorder.LOWERED, new Color(0, 0, 0), null),
-                "Exercise's Questions", TitledBorder.CENTER, TitledBorder.TOP,
-                new Font("Tahoma", Font.PLAIN, 20)));
-        this.lowerCenterPanel.add(this.questionTablePanel, BorderLayout.CENTER);
+		this.txtFilter = new JTextField();
+		this.filterPanel.add(this.txtFilter);
+		this.txtFilter.setColumns(14);
 
-        DefaultTableModel questionModel = new DefaultTableModel();
-        String[] questionHeads = { "Question Type", "Question", "Tags" };
-        questionModel.setRowCount(1);
-        questionModel.setColumnIdentifiers(questionHeads);
-        questionModel.setValueAt("Multiple Choice", 0, 0);
-        questionModel.setValueAt("What is the square root of -1?", 0, 1);
-        questionModel.setValueAt("square-root, math", 0, 2);
+		this.btnFilter = new JButton("Filter");
+		this.filterPanel.add(this.btnFilter);
+		this.centerPanel.add(this.filterPanel, BorderLayout.SOUTH);
+	}
 
-        this.questionTable.setModel(questionModel);
+	/**
+	 * Builds the center panel with a JTable for all exercises.
+	 */
+	private void buildCenterPanel() {
+		this.centerPanel = new JPanel(new BorderLayout());
+		this.centerPanel.setBackground(UIColors.lightBlue);
+		this.centerPanel.setBorder(new TitledBorder(new EtchedBorder(
+				EtchedBorder.LOWERED, new Color(0, 0, 0), null), "Exercises",
+				TitledBorder.CENTER, TitledBorder.TOP, new Font("Tahoma",
+						Font.PLAIN, 20)));
 
-        JScrollPane questionScroll = new JScrollPane(this.questionTable);
-        questionScroll.getViewport().setBackground(UIColors.white);
+		this.exerciseTable = new JTable();
+		this.exerciseTable.setModel(this.presenter.getExerciseTableModel());
 
-        this.questionTablePanel.add(questionScroll);
-    }
+		buildFilterPanel();
 
-    /**
-     * Builds the filter panel for filtering exercises.
-     */
-    private void buildFilterPanel() {
-        this.filterPanel = new JPanel();
-        this.filterPanel.setBorder(new TitledBorder(new EtchedBorder(
-                EtchedBorder.LOWERED, new Color(0, 0, 0), null),
-                "Filter Exercises", TitledBorder.CENTER, TitledBorder.TOP,
-                new Font("Tahoma", Font.PLAIN, 20)));
-        this.filterPanel.setBackground(UIColors.lightBlue);
+		JScrollPane exerciseScroll = new JScrollPane(this.exerciseTable);
+		exerciseScroll.getViewport().setBackground(UIColors.white);
+		this.centerPanel.add(exerciseScroll, BorderLayout.CENTER);
 
-        JLabel lblFilter = new JLabel("Filter");
-        this.filterPanel.add(lblFilter);
+		this.exerciseManagerPanel.add(this.centerPanel);
+	}
 
-        this.filterBox = new JComboBox<>();
-        this.filterBox.setModel(new DefaultComboBoxModel<>(this.FILTER));
-        this.filterPanel.add(this.filterBox);
+	/**
+	 * Adds an ActionListener of type ExerciseManagerUIListener to all the
+	 * JButtons from the exerciseManagerPanel.
+	 */
+	private void addActionListeners() {
+		ExerciseManagerUIListener listener = new ExerciseManagerUIListener();
+		this.btnAddExercise.addActionListener(listener);
+		this.btnDeleteExercise.addActionListener(listener);
+		this.btnEditExercise.addActionListener(listener);
+		this.btnHelp.addActionListener(listener);
+		this.btnFilter.addActionListener(listener);
+		this.btnDuplicateExercise.addActionListener(listener);
+		this.exerciseTable.addMouseListener(listener);
+	}
 
-        JLabel lblBy = new JLabel("by");
-        this.filterPanel.add(lblBy);
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
 
-        this.txtFilter = new JTextField();
-        this.filterPanel.add(this.txtFilter);
-        this.txtFilter.setColumns(14);
+	/**
+	 * The ExerciseManagerUIListener class is the class in charge of listening
+	 * for events happening in the ExerciseManagerUI (e.g. an user clicking the
+	 * help button). When an event occurs the ExerciseManagerUIListener will
+	 * send a signal to the ExerciseManagerPresenter which will in return act
+	 * upon the event.
+	 * 
+	 * @author Morten Faarkrog
+	 * @version 0.1
+	 * @see ActionListener
+	 * @see ExerciseManagerPresenter
+	 * @date 5. December, 2013
+	 */
+	private class ExerciseManagerUIListener implements ActionListener,
+			MouseListener {
 
-        this.btnFilter = new JButton("Filter");
-        this.filterPanel.add(this.btnFilter);
-        this.lowerCenterPanel.add(this.filterPanel, BorderLayout.NORTH);
-    }
-
-    /**
-     * Builds the upper center panel with a JTable for all exercises.
-     */
-    private void buildUpperCenterPanel() {
-        JPanel upperCenterPanel = new JPanel(new BorderLayout());
-        upperCenterPanel.setBackground(UIColors.lightBlue);
-        upperCenterPanel.setBorder(new TitledBorder(new EtchedBorder(
-                EtchedBorder.LOWERED, new Color(0, 0, 0), null), "Exercises",
-                TitledBorder.CENTER, TitledBorder.TOP, new Font("Tahoma",
-                        Font.PLAIN, 20)));
-
-        this.exerciseTable = new JTable();
-        DefaultTableModel exerciseModel = new DefaultTableModel();
-        String[] exerciseHeads = { "Course", "Author", "Exercise Name",
-                "Is Test" };
-        exerciseModel.setRowCount(1);
-        exerciseModel.setColumnIdentifiers(exerciseHeads);
-        exerciseModel.setValueAt("Software Construction", 0, 0);
-        exerciseModel.setValueAt("John Smith", 0, 1);
-        exerciseModel.setValueAt("Discrete Mathematics in Computer Science", 0,
-                2);
-        exerciseModel.setValueAt("X", 0, 3);
-        this.exerciseTable.setModel(exerciseModel);
-
-        JScrollPane exerciseScroll = new JScrollPane(this.exerciseTable);
-        exerciseScroll.getViewport().setBackground(UIColors.white);
-        upperCenterPanel.add(exerciseScroll, BorderLayout.CENTER);
-
-        this.centerPanel.add(upperCenterPanel);
-    }
-
-    /**
-     * Adds an ActionListener of type ExerciseManagerUIListener to all the
-     * JButtons from the exerciseManagerPanel.
-     */
-    private void addActionListeners() {
-        ExerciseManagerUIListener listener = new ExerciseManagerUIListener();
-        this.btnAddExercise.addActionListener(listener);
-        this.btnDeleteExercise.addActionListener(listener);
-        this.btnEditExercise.addActionListener(listener);
-        this.btnHelp.addActionListener(listener);
-        this.btnFilter.addActionListener(listener);
-        this.btnDuplicateExercise.addActionListener(listener);
-    }
-
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-
-    /**
-     * The ExerciseManagerUIListener class is the class in charge of listening
-     * for events happening in the ExerciseManagerUI (e.g. an user clicking the
-     * help button). When an event occurs the ExerciseManagerUIListener will
-     * send a signal to the ExerciseManagerPresenter which will in return act
-     * upon the event.
-     * 
-     * @author Morten Faarkrog
-     * @version 0.1
-     * @see ActionListener
-     * @see ExerciseManagerPresenter
-     * @date 5. December, 2013
-     */
-    private class ExerciseManagerUIListener implements ActionListener {
-
-        public ExerciseManagerUIListener() {
-			// Empty Constructor 
+		public ExerciseManagerUIListener() {
+			// Empty Constructor
 		}
 
 		@Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == ExerciseManagerUI.this.btnAddExercise) {
-                MainFrame.updateFrame(
-                        new ManageExerciseInfoUI().getManageExerciseInfoUI(),
-                        "Manage Exercise Info");
-            }
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == ExerciseManagerUI.this.btnAddExercise) {
+				MainFrame.updateFrame(
+						new ManageExerciseInfoUI().getManageExerciseInfoUI(),
+						"Manage Exercise Info");
+			}
 
-            else if (e.getSource() == ExerciseManagerUI.this.btnDeleteExercise) {
+			else if (e.getSource() == ExerciseManagerUI.this.btnDeleteExercise) {
+				int reply = JOptionPane.showConfirmDialog(null,
+						"Are you sure you want to perform a Delete?",
+						"Delete Confirmation", JOptionPane.YES_NO_OPTION);
+				if (reply == JOptionPane.YES_OPTION) {
+					ExerciseManagerUI.this.presenter.delete();
+				}
+			}
 
-            }
+			else if (e.getSource() == ExerciseManagerUI.this.btnEditExercise) {
+				// TODO
+			}
 
-            else if (e.getSource() == ExerciseManagerUI.this.btnEditExercise) {
+			else if (e.getSource() == ExerciseManagerUI.this.btnHelp) {
+				JOptionPane.showMessageDialog(null,
+						HelpPresenter.getExerciseManagerHelp(),
+						HelpPresenter.getExerciseManagerTitle(),
+						JOptionPane.PLAIN_MESSAGE, HelpPresenter.getHelpIcon());
+			}
 
-            }
+			else if (e.getSource() == ExerciseManagerUI.this.btnFilter) {
+				String column = ExerciseManagerUI.this.filterBox
+						.getSelectedItem().toString();
+				String by = ExerciseManagerUI.this.txtFilter.getText();
 
-            else if (e.getSource() == ExerciseManagerUI.this.btnHelp) {
-                JOptionPane.showMessageDialog(null,
-                        HelpPresenter.getExerciseManagerHelp(),
-                        HelpPresenter.getExerciseManagerTitle(),
-                        JOptionPane.PLAIN_MESSAGE, HelpPresenter.getHelpIcon());
-            }
+				ExerciseManagerUI.this.presenter.filter(column, by);
+			}
 
-            else if (e.getSource() == ExerciseManagerUI.this.btnFilter) {
+			else if (e.getSource() == ExerciseManagerUI.this.btnDuplicateExercise) {
+				ExerciseManagerUI.this.presenter.duplicate();
+			}
+		}
 
-            }
-            
-            else if (e.getSource() == ExerciseManagerUI.this.btnDuplicateExercise) {
-                
-            }
-        }
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			if(arg0.getSource() == ExerciseManagerUI.this.exerciseTable) {
+			ExerciseManagerUI.this.presenter
+					.setSelectedExercise(ExerciseManagerUI.this.exerciseTable
+							.rowAtPoint(arg0.getPoint()));
+			}
+		}
 
-    }
+		@Override
+		public void mouseEntered(MouseEvent arg0) { /* Do nothing */
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) { /* Do nothing */
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) { /* Do nothing */
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) { /* Do nothing */
+		}
+
+	}
 
 }
