89e9c494db77454d0e08708e7f5ba220125284f1
warreq <brandon.s.lucas@gmail.com>
2013-12-18 20:12:10 +0100

    QuestionManager works

--- MBOEasyTeach/src/com/easyTeach/client/presenter/QuestionManagerPresenter.java
+++ MBOEasyTeach/src/com/easyTeach/client/presenter/QuestionManagerPresenter.java
@@ -1,5 +1,251 @@
 package com.easyTeach.client.presenter;
 
+import com.easyTeach.client.network.EasyTeachClient;
+import com.easyTeach.common.entity.Answer;
+import com.easyTeach.common.entity.Question;
+import com.easyTeach.common.entity.Resource;
+import com.easyTeach.common.entity.ResourceSet;
+import com.easyTeach.common.network.Action;
+import com.easyTeach.common.network.Action.ActionType;
+import com.easyTeach.common.network.Request;
+import com.easyTeach.common.network.Response.ResponseStatus;
+import com.easyTeach.common.network.Session;
+
+/**
+ * The QuestionManagerPresenter communicates with the domain logic on behalf of
+ * the QuestionManagerUI, providing all of its logic.
+ * 
+ * <p>
+ * The Listeners in the UI class call the relevant methods in the Presenter in
+ * order to update and retrieve information from the domain logic. This is an
+ * implementation of the Model View Presenter pattern.
+ * </p>
+ * 
+ * @author Brandon Lucas
+ * @version 1.0
+ * @date 14. December, 2013
+ */
 public class QuestionManagerPresenter {
-	//TODO
+
+	private QuestionTableModel questionTable;
+	private AnswerTableModel answerTable;
+	protected ResourceSet filteredQuestionSet;
+	protected ResourceSet questionSet;
+	protected ResourceSet answers;
+	private EasyTeachClient client;
+	private Question currentlySelectedQuestion;
+	private boolean isFiltered;
+
+	private String[] answerColumnHeaders = { "Answer", "Is Correct" };
+	private String[] questionColumnHeaders = { "Question Type", "Question" };
+
+	/**
+	 * Initializes sets and table models
+	 */
+	public QuestionManagerPresenter() {
+		this.questionSet = new ResourceSet();
+		this.answers = new ResourceSet();
+		this.questionTable = new QuestionTableModel(this.questionColumnHeaders,
+				this.questionSet);
+
+		this.answerTable = new AnswerTableModel(this.answerColumnHeaders,
+				this.answers);
+
+		refreshTable();
+	}
+
+	public QuestionTableModel getQuestionTableModel() {
+		return this.questionTable;
+	}
+
+	public AnswerTableModel getAnswerTableModel() {
+		return this.answerTable;
+	}
+
+	public void setSelectedQuestion(int row) {
+		this.currentlySelectedQuestion = (Question) this.questionTable
+				.getResourceAtRow(row);
+		getAnswers(this.currentlySelectedQuestion);
+	}
+
+	public Question getSelectedQuestion() {
+		return this.currentlySelectedQuestion;
+	}
+
+	private ResourceSet getAnswers(Question question) {
+		Action readAnswers = new Action(ActionType.READ, "answer");
+		Request getAnswers = new Request(Session.getInstance(),
+				readAnswers, question);
+
+		this.client = new EasyTeachClient(getAnswers);
+		this.client.run();
+		ResourceSet answerSet = (ResourceSet) this.client.getResponse()
+				.getResponse();
+
+		this.answerTable.refreshData(answerSet);
+		this.answerTable.fireTableDataChanged();
+
+		return answerSet;
+	}
+
+	/**
+	 * Sends a {@link Request} to the Server, updating the Presenter's set of
+	 * Questions.
+	 * 
+	 * <p>
+	 * It checks if the data in the table should be filtered, and updates the
+	 * Table Model accordingly, before firing the dataChanged event and forcing
+	 * the table in the UI to update.
+	 * </p>
+	 */
+	private void refreshTable() {
+
+		Action toDo = new Action(ActionType.READ, "all");
+
+		Request getQuestions = new Request(Session.getInstance(), toDo,
+				new Question());
+		this.client = new EasyTeachClient(getQuestions);
+		this.client.run();
+		this.client.getResponse();
+		if (this.client.getResponse().getStatus() != ResponseStatus.FAILURE) {
+			this.questionSet = (ResourceSet) this.client.getResponse()
+					.getResponse();
+		}
+		if (this.isFiltered) {
+			this.questionTable.refreshData(this.filteredQuestionSet);
+		} else {
+			this.questionTable.refreshData(this.questionSet);
+		}
+
+		this.questionTable.fireTableDataChanged();
+	}
+
+	/**
+	 * Logic for filtering the table containing the selection of questions by a
+	 * particular string, in a particular column.
+	 * 
+	 * <p>
+	 * Right now, this consists of maintaining a separate, filtered set. This is
+	 * somewhat slow. Later, new logic should be added, using RowFilter.
+	 * </p>
+	 * 
+	 * @param column
+	 *            The column in the table which should be considered for
+	 *            filtering
+	 * @param by
+	 *            The string by which the aforementioned column should be
+	 *            filtered.
+	 */
+	public void filter(String column, String by) {
+		if (by.equals("")) {
+			this.isFiltered = false;
+		} else {
+			this.isFiltered = true;
+			this.filteredQuestionSet = new ResourceSet();
+			switch (column) {
+			case "Question Type":
+				for (Resource r : this.questionSet) {
+					Question q = (Question) r;
+					if (q.getQuestionType().contains(by)) {
+						this.filteredQuestionSet.add(q);
+					}
+				}
+				break;
+			}
+		}
+		this.refreshTable();
+		this.questionTable.refreshData(this.filteredQuestionSet);
+		this.questionTable.fireTableDataChanged();
+	}
+
+	/**
+	 * Removes a Question from the QuestionTable.
+	 */
+	public void delete() {
+		if (this.currentlySelectedQuestion != null) {
+			Action rm = new Action(ActionType.DELETE);
+			Request rmExercise = new Request(Session.getInstance(), rm,
+					this.currentlySelectedQuestion);
+
+			this.client = new EasyTeachClient(rmExercise);
+			this.client.run();
+			this.refreshTable();
+			this.questionTable.refreshData(this.questionSet);
+			this.questionTable.fireTableDataChanged();
+		}
+	}
+
+	/**
+	 * 
+	 * @author Brandon Lucas
+	 * @version 1.0
+	 * @date 14 December, 2013
+	 * 
+	 *       Presenter-specific implementation of the abstract
+	 *       DisplayTableModel.
+	 * 
+	 *       <p>
+	 *       Implementation of the DisplayTableModel on a per-Presenter basis
+	 *       allows us to interact rows in the table model as actual domain
+	 *       objects, specific to the demands of the particular user-interface.
+	 *       </p>
+	 * 
+	 */
+	private class QuestionTableModel extends DisplayTableModel {
+		public QuestionTableModel(String[] columnHeaders, ResourceSet resources) {
+			super(columnHeaders, resources);
+		}
+
+		private static final long serialVersionUID = -2996998684634631118L;
+
+		@Override
+		public String getValueAt(int row, int column) {
+			Question q = (Question) this.tableData.get(row);
+			switch (this.getColumnName(column)) {
+			case "Question Type":
+				return q.getQuestionType();
+			case "Question":
+				return q.getQuestion();
+			default:
+				return new String();
+			}
+		}
+	}
+
+	/**
+	 * 
+	 * @author Brandon Lucas
+	 * @version 1.0
+	 * @date 14 December, 2013
+	 * 
+	 *       Presenter-specific implementation of the abstract
+	 *       DisplayTableModel.
+	 * 
+	 *       <p>
+	 *       Implementation of the DisplayTableModel on a per-Presenter basis
+	 *       allows us to interact rows in the table model as actual domain
+	 *       objects, specific to the demands of the particular user-interface.
+	 *       </p>
+	 * 
+	 */
+	private class AnswerTableModel extends DisplayTableModel {
+		public AnswerTableModel(String[] columnHeaders, ResourceSet resources) {
+			super(columnHeaders, resources);
+		}
+
+		private static final long serialVersionUID = -2996998684634631118L;
+
+		@Override
+		public String getValueAt(int row, int column) {
+			Answer a = (Answer) this.tableData.get(row);
+			switch (this.getColumnName(column)) {
+			case "Answer":
+				return a.getAnswer();
+			case "Is Correct":
+				return Boolean.toString(a.getIsCorrect());
+			default:
+				return new String();
+			}
+		}
+	}
 }
--- MBOEasyTeach/src/com/easyTeach/client/ui/QuestionManagerUI.java
+++ MBOEasyTeach/src/com/easyTeach/client/ui/QuestionManagerUI.java
@@ -7,6 +7,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
@@ -19,15 +21,13 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
-import javax.swing.table.DefaultTableModel;
 
-import com.easyTeach.client.presenter.DisplayTableModel;
 import com.easyTeach.client.presenter.HelpPresenter;
 import com.easyTeach.client.presenter.QuestionManagerPresenter;
 import com.easyTeach.common.ui.UIColors;
 
 /**
- * The QuestionManagerUI class constructs a JPanel with all the different 
+ * The QuestionManagerUI class constructs a JPanel with all the different
  * JComponents needed to maintain questions (Add, edit and delete). For every
  * 
  * 
@@ -36,243 +36,269 @@ import com.easyTeach.common.ui.UIColors;
  * @date 5. December, 2013
  */
 
-public class QuestionManagerUI  {
+public class QuestionManagerUI {
 
-    private final String[] FILTER = {"Tag", "Points", "Question"};
-    private JPanel questionManagerPanel;
-    JButton btnHelp;
-    JButton btnEditQuestion;
-    JButton btnDeleteQuestion;
-    JButton btnAddQuestion;
-    private JPanel centerPanel;
-    private JPanel lowerCenterPanel;
-    JTable answerTable;
-    JTable questionTable;
-    private JPanel filterPanel;
-    JComboBox<String> filterBox;
-    JButton btnFilter;
-    private JPanel answerTablePanel;
-    JTextField txtFilter;
-    
-    /**
-     * Constructor for building the questionManagerPanel. The panel is built 
-     * by calling the buildPanel method. The constructor also calls a method
-     * adding actionListeners to all buttons.
-     */
-    public QuestionManagerUI() {
-        buildPanel();
-        addActionListeners();
-    }
-    
-    /**
-     * The getQuestionManagerUI returns the JPanel containing all the 
-     * components needed to maintain questions.
-     * 
-     * @return the JPanel, questionManagerPanel. Meant to be used in 
-     * another frame.
-     */
-    public JPanel getQuestionManagerUI() {
-        return this.questionManagerPanel;
-    }
-    
-    /**
-     * The method responsible for building the questionManagerPanel. The panel
-     * consists of all the JComponenets needed to maintain questions.
-     */
-    public void buildPanel() {
-        this.questionManagerPanel = new JPanel(new BorderLayout());
-        this.questionManagerPanel.setBackground(UIColors.lightBlue);
-        
-        buildCenterPanel();
-        buildSouthPanel();               
-    }
-    
-    /**
-     * Method for creating the south panel with buttons for getting help and 
-     * editing/deleting/adding questions.
-     */
-    private void buildSouthPanel() {
-        JPanel southButtonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-        southButtonPanel.setBackground(UIColors.darkBlue);
-        
-        this.btnHelp = new JButton("Help");
-        southButtonPanel.add(this.btnHelp);
-        
-        this.btnEditQuestion = new JButton("Edit Question");
-        southButtonPanel.add(this.btnEditQuestion);
-        
-        this.btnDeleteQuestion = new JButton("Delete Question");
-        southButtonPanel.add(this.btnDeleteQuestion);
-        
-        this.btnAddQuestion = new JButton("Add Question");
-        southButtonPanel.add(this.btnAddQuestion);
+	private final String[] FILTER = { "Question Type" };
+	private JPanel questionManagerPanel;
+	JButton btnHelp;
+	JButton btnEditQuestion;
+	JButton btnDeleteQuestion;
+	JButton btnAddQuestion;
+	private JPanel centerPanel;
+	private JPanel lowerCenterPanel;
+	JTable answerTable;
+	JTable questionTable;
+	private JPanel filterPanel;
+	JComboBox<String> filterBox;
+	JButton btnFilter;
+	private JPanel answerTablePanel;
+	JTextField txtFilter;
+	QuestionManagerPresenter presenter;
 
-        this.questionManagerPanel.add(southButtonPanel, BorderLayout.SOUTH);
-    }
-    
-    /**
-     * Builds the center panel consisting of two inner panels.
-     * One for an answer table showing which questions relate to answers
-     * and another containing a panel to filter questions.
-     */
-    private void buildCenterPanel() {
-        this.centerPanel = new JPanel(new GridLayout(2, 1));
-        this.questionManagerPanel.add(this.centerPanel, BorderLayout.CENTER);
-        
-        buildUpperCenterPanel();
-        
-        this.lowerCenterPanel = new JPanel(new BorderLayout());
-        buildFilterPanel();
-        buildAnswerTablePanel();
-        
-        this.centerPanel.add(this.lowerCenterPanel);
-    }
-    
-    /**
-     * Builds a panel with a table for answers for a selected question.
-     */
-    private void buildAnswerTablePanel() {
-        this.answerTablePanel = new JPanel(new BorderLayout());
-        this.answerTable = new JTable();
+	/**
+	 * Constructor for building the questionManagerPanel. The panel is built by
+	 * calling the buildPanel method. The constructor also calls a method adding
+	 * actionListeners to all buttons.
+	 */
+	public QuestionManagerUI() {
+		this.presenter = new QuestionManagerPresenter();
+		buildPanel();
+		addActionListeners();
+	}
 
-        this.answerTablePanel.setBackground(UIColors.lightBrown);
-        this.answerTablePanel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, 
-                new Color(0, 0, 0), null), "Question's Answers", TitledBorder.CENTER, 
-                TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 20)));     
-        this.lowerCenterPanel.add(this.answerTablePanel, BorderLayout.CENTER);
-        
-        DefaultTableModel answerModel = new DefaultTableModel();
-        String[] answerHeads = {"Correct", "Answer"};
-        answerModel.setRowCount(1);
-        answerModel.setColumnIdentifiers(answerHeads);
-        answerModel.setValueAt("X", 0, 0);
-        answerModel.setValueAt("42", 0, 1);
-        
-        this.answerTable.setModel(answerModel);
-        
-        JScrollPane answerScroll = new JScrollPane(this.answerTable);
-        answerScroll.getViewport().setBackground(UIColors.white);
+	/**
+	 * The getQuestionManagerUI returns the JPanel containing all the components
+	 * needed to maintain questions.
+	 * 
+	 * @return the JPanel, questionManagerPanel. Meant to be used in another
+	 *         frame.
+	 */
+	public JPanel getQuestionManagerUI() {
+		return this.questionManagerPanel;
+	}
 
-        this.answerTablePanel.add(answerScroll);
-    }
-    
-    /**
-     * Builds the filter panel for filtering questions.
-     */
-    private void buildFilterPanel() {
-        this.filterPanel = new JPanel();
-        this.filterPanel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, 
-                new Color(0, 0, 0), null), "Filter Questions", TitledBorder.CENTER, 
-                TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 20)));
-        this.filterPanel.setBackground(UIColors.lightBlue);
-        
-        JLabel lblFilter = new JLabel("Filter");
-        this.filterPanel.add(lblFilter);
-        
-        this.filterBox = new JComboBox<>();
-        this.filterBox.setModel(new DefaultComboBoxModel<>(this.FILTER));
-        this.filterPanel.add(this.filterBox);
-        
-        JLabel lblBy = new JLabel("by");
-        this.filterPanel.add(lblBy);
-        
-        this.txtFilter = new JTextField();
-        this.filterPanel.add(this.txtFilter);
-        this.txtFilter.setColumns(14);
-        
-        this.btnFilter = new JButton("Filter");
-        this.filterPanel.add(this.btnFilter);
-        this.lowerCenterPanel.add(this.filterPanel, BorderLayout.NORTH);
-    }
-    
-    /**
-     * Builds the upper center panel with a JTable for all questions.
-     */
-    private void buildUpperCenterPanel() {
-        JPanel upperCenterPanel = new JPanel(new BorderLayout());
-        upperCenterPanel.setBackground(UIColors.lightBlue);
-        upperCenterPanel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, 
-                new Color(0, 0, 0), null), "Questions", TitledBorder.CENTER, 
-                TitledBorder.TOP, new Font("Tahoma", Font.PLAIN, 20)));
-        
-        this.questionTable = new JTable();
-        DefaultTableModel questionModel = new DefaultTableModel();
-        String[] questionHeads = {"Question No", "Question Type", "Question", "Points", "Tags"};
-        questionModel.setRowCount(1);
-        questionModel.setColumnIdentifiers(questionHeads);
-        questionModel.setValueAt("134", 0, 0);
-        questionModel.setValueAt("Multiple Choice", 0, 1);
-        questionModel.setValueAt("What is the meaning of life?", 0, 2);
-        questionModel.setValueAt("1337", 0, 3);
-        questionModel.setValueAt("meaning, life", 0, 4);
-        this.questionTable.setModel(questionModel);
-        
-        JScrollPane questionScroll = new JScrollPane(this.questionTable);
-        questionScroll.getViewport().setBackground(UIColors.white);
-        upperCenterPanel.add(questionScroll, BorderLayout.CENTER);
+	/**
+	 * The method responsible for building the questionManagerPanel. The panel
+	 * consists of all the JComponenets needed to maintain questions.
+	 */
+	public void buildPanel() {
+		this.questionManagerPanel = new JPanel(new BorderLayout());
+		this.questionManagerPanel.setBackground(UIColors.lightBlue);
 
-        this.centerPanel.add(upperCenterPanel);
-    }
-    
-    /**
-     * Adds an ActionListener of type QuestionManagerUIListener to all the 
-     * JButtons from the exerciseManagerPanel.
-     */
-    private void addActionListeners() {
-        QuestionManagerUIListener listener = new QuestionManagerUIListener();
-        this.btnAddQuestion.addActionListener(listener);
-        this.btnDeleteQuestion.addActionListener(listener);
-        this.btnEditQuestion.addActionListener(listener);
-        this.btnHelp.addActionListener(listener);
-        this.btnFilter.addActionListener(listener);
-    }
+		buildCenterPanel();
+		buildSouthPanel();
+	}
 
-    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-    
-    /**
-     * The QuestionManagerUIListener class is the class in charge of listening
-     * for events happening in the QuestionManagerUI (e.g. an user clicking the 
-     * help button). When an event occurs the QuestionManagerUIListener will 
-     * send a signal to the QuestionManagerPresenter which will in return act 
-     * upon the event.
-     * 
-     * @author Morten Faarkrog
-     * @version 0.1
-     * @see ActionListener
-     * @see QuestionManagerPresenter
-     * @date 5. December, 2013
-     */
-    private class QuestionManagerUIListener implements ActionListener {
+	/**
+	 * Method for creating the south panel with buttons for getting help and
+	 * editing/deleting/adding questions.
+	 */
+	private void buildSouthPanel() {
+		JPanel southButtonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		southButtonPanel.setBackground(UIColors.darkBlue);
 
-        public QuestionManagerUIListener() {
-			// Empty Constructor 
+		this.btnHelp = new JButton("Help");
+		southButtonPanel.add(this.btnHelp);
+
+		this.btnEditQuestion = new JButton("Edit Question");
+		southButtonPanel.add(this.btnEditQuestion);
+
+		this.btnDeleteQuestion = new JButton("Delete Question");
+		southButtonPanel.add(this.btnDeleteQuestion);
+
+		this.btnAddQuestion = new JButton("Add Question");
+		southButtonPanel.add(this.btnAddQuestion);
+
+		this.questionManagerPanel.add(southButtonPanel, BorderLayout.SOUTH);
+	}
+
+	/**
+	 * Builds the center panel consisting of two inner panels. One for an answer
+	 * table showing which questions relate to answers and another containing a
+	 * panel to filter questions.
+	 */
+	private void buildCenterPanel() {
+		this.centerPanel = new JPanel(new GridLayout(2, 1));
+		this.questionManagerPanel.add(this.centerPanel, BorderLayout.CENTER);
+
+		buildUpperCenterPanel();
+
+		this.lowerCenterPanel = new JPanel(new BorderLayout());
+		buildFilterPanel();
+		buildAnswerTablePanel();
+
+		this.centerPanel.add(this.lowerCenterPanel);
+	}
+
+	/**
+	 * Builds a panel with a table for answers for a selected question.
+	 */
+	private void buildAnswerTablePanel() {
+		this.answerTablePanel = new JPanel(new BorderLayout());
+		this.answerTable = new JTable();
+
+		this.answerTablePanel.setBackground(UIColors.lightBrown);
+		this.answerTablePanel.setBorder(new TitledBorder(new EtchedBorder(
+				EtchedBorder.LOWERED, new Color(0, 0, 0), null),
+				"Question's Answers", TitledBorder.CENTER, TitledBorder.TOP,
+				new Font("Tahoma", Font.PLAIN, 20)));
+		this.lowerCenterPanel.add(this.answerTablePanel, BorderLayout.CENTER);
+
+		this.answerTable.setModel(this.presenter.getAnswerTableModel());
+
+		JScrollPane answerScroll = new JScrollPane(this.answerTable);
+		answerScroll.getViewport().setBackground(UIColors.white);
+
+		this.answerTablePanel.add(answerScroll);
+	}
+
+	/**
+	 * Builds the filter panel for filtering questions.
+	 */
+	private void buildFilterPanel() {
+		this.filterPanel = new JPanel();
+		this.filterPanel.setBorder(new TitledBorder(new EtchedBorder(
+				EtchedBorder.LOWERED, new Color(0, 0, 0), null),
+				"Filter Questions", TitledBorder.CENTER, TitledBorder.TOP,
+				new Font("Tahoma", Font.PLAIN, 20)));
+		this.filterPanel.setBackground(UIColors.lightBlue);
+
+		JLabel lblFilter = new JLabel("Filter");
+		this.filterPanel.add(lblFilter);
+
+		this.filterBox = new JComboBox<>();
+		this.filterBox.setModel(new DefaultComboBoxModel<>(this.FILTER));
+		this.filterPanel.add(this.filterBox);
+
+		JLabel lblBy = new JLabel("by");
+		this.filterPanel.add(lblBy);
+
+		this.txtFilter = new JTextField();
+		this.filterPanel.add(this.txtFilter);
+		this.txtFilter.setColumns(14);
+
+		this.btnFilter = new JButton("Filter");
+		this.filterPanel.add(this.btnFilter);
+		this.lowerCenterPanel.add(this.filterPanel, BorderLayout.NORTH);
+	}
+
+	/**
+	 * Builds the upper center panel with a JTable for all questions.
+	 */
+	private void buildUpperCenterPanel() {
+		JPanel upperCenterPanel = new JPanel(new BorderLayout());
+		upperCenterPanel.setBackground(UIColors.lightBlue);
+		upperCenterPanel.setBorder(new TitledBorder(new EtchedBorder(
+				EtchedBorder.LOWERED, new Color(0, 0, 0), null), "Questions",
+				TitledBorder.CENTER, TitledBorder.TOP, new Font("Tahoma",
+						Font.PLAIN, 20)));
+
+		this.questionTable = new JTable();
+		this.questionTable.setModel(this.presenter.getQuestionTableModel());
+
+		JScrollPane questionScroll = new JScrollPane(this.questionTable);
+		questionScroll.getViewport().setBackground(UIColors.white);
+		upperCenterPanel.add(questionScroll, BorderLayout.CENTER);
+
+		this.centerPanel.add(upperCenterPanel);
+	}
+
+	/**
+	 * Adds an ActionListener of type QuestionManagerUIListener to all the
+	 * JButtons from the exerciseManagerPanel.
+	 */
+	private void addActionListeners() {
+		QuestionManagerUIListener listener = new QuestionManagerUIListener();
+		this.btnAddQuestion.addActionListener(listener);
+		this.btnDeleteQuestion.addActionListener(listener);
+		this.btnEditQuestion.addActionListener(listener);
+		this.btnHelp.addActionListener(listener);
+		this.btnFilter.addActionListener(listener);
+		this.questionTable.addMouseListener(listener);
+	}
+
+	// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+	/**
+	 * The QuestionManagerUIListener class is the class in charge of listening
+	 * for events happening in the QuestionManagerUI (e.g. an user clicking the
+	 * help button). When an event occurs the QuestionManagerUIListener will
+	 * send a signal to the QuestionManagerPresenter which will in return act
+	 * upon the event.
+	 * 
+	 * @author Morten Faarkrog
+	 * @version 0.1
+	 * @see ActionListener
+	 * @see QuestionManagerPresenter
+	 * @date 5. December, 2013
+	 */
+	private class QuestionManagerUIListener implements ActionListener,
+			MouseListener {
+
+		public QuestionManagerUIListener() {
+			// Empty Constructor
 		}
 
 		@Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == QuestionManagerUI.this.btnAddQuestion) {
-                
-            }
-            
-            else if (e.getSource() == QuestionManagerUI.this.btnDeleteQuestion) {
-                
-            }
-            
-            else if (e.getSource() == QuestionManagerUI.this.btnEditQuestion) {
-                
-            }
-            
-            else if (e.getSource() == QuestionManagerUI.this.btnHelp) {
-                JOptionPane.showMessageDialog(null, HelpPresenter.getQuestionManagerHelp(), 
-                        HelpPresenter.getQuestionManagerTitle(), JOptionPane.PLAIN_MESSAGE, 
-                        HelpPresenter.getHelpIcon());
-            }
-            
-            else if (e.getSource() == QuestionManagerUI.this.btnFilter) {
-                
-            }
-        }
-        
-    }
-    
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == QuestionManagerUI.this.btnAddQuestion) {
+				//
+			}
+
+			else if (e.getSource() == QuestionManagerUI.this.btnDeleteQuestion) {
+				//
+			}
+
+			else if (e.getSource() == QuestionManagerUI.this.btnEditQuestion) {
+				//
+			}
+
+			else if (e.getSource() == QuestionManagerUI.this.btnHelp) {
+				JOptionPane.showMessageDialog(null,
+						HelpPresenter.getQuestionManagerHelp(),
+						HelpPresenter.getQuestionManagerTitle(),
+						JOptionPane.PLAIN_MESSAGE, HelpPresenter.getHelpIcon());
+			}
+
+			else if (e.getSource() == QuestionManagerUI.this.btnFilter) {
+				String column = QuestionManagerUI.this.filterBox
+						.getSelectedItem().toString();
+				String by = QuestionManagerUI.this.txtFilter.getText();
+
+				QuestionManagerUI.this.presenter.filter(column, by);
+			}
+		}
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			if (arg0.getSource() == QuestionManagerUI.this.questionTable) {
+				QuestionManagerUI.this.presenter
+						.setSelectedQuestion(QuestionManagerUI.this.questionTable
+								.rowAtPoint(arg0.getPoint()));
+			}
+
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			//
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			//
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			//
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			//
+		}
+
+	}
+
 }
